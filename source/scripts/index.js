488f2e21af44a2d464a30f1b979c62b95957e44d
AlexanderNeret <164453854+AlexanderNeret@users.noreply.github.com>
2024-09-26 00:00:14 +0700

    делает планшетную версию

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,20 @@
 /* в этот файл добавляет скрипты*/
+// eslint-disable-next-line no-unused-vars
+
+const button = document.querySelector('.main-nav__toggle');
+
+const nav = document.querySelector('nav');
+
+nav.classList.remove('.no-js');
+
+button.addEventListener('click', () => {
+  nav.classList.toggle('main-nav--opened');
+  if(nav.classList.contains('main-nav--opened')){
+    button.classList.remove('main-nav__toggle--burger');
+    button.classList.add('main-nav__toggle--open');
+  } else{
+    button.classList.add('main-nav__toggle--burger');
+    button.classList.remove('main-nav__toggle--open');
+  }
+});
+
